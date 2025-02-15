a3c516abc539dc22e3940346c42f787ad6a7e02e
Tobias Federico Alvarez <138098809+tobiasalvarez@users.noreply.github.com>
2025-02-15 14:28:06 -0300

    commit iniciando projeto

---
+++ SistemaEscola2/SistemaEscola2/src/test/java/com/example/SistemaEscola2/SistemaEscola2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SistemaEscola2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaEscola2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
